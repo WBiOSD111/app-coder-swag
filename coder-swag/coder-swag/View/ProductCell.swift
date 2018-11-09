3b797bb9ac31395e309557e2b6574fc5bb0a0022
grav0211 <grav0211@algonquinlive.com>
2018-11-09 12:55:25 -0500

    Added product collection view to the products view controller

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Alexandre Gravelle on 2018-11-09.
+//  Copyright © 2018 Alexandre Gravelle. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
